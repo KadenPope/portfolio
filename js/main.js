ec24b3b6852fe2a137a9b1687aa3d0930ca05ef4
Kaden Pope <kadencpope@outlook.com>
2020-04-26 14:06:42 -0700

    Added top navigation bar

--- js/main.js
+++ js/main.js
@@ -26,36 +26,3 @@ function toggleMenu() {
         showMenu = false;
     }
 }
-
-function starGenerator(el, starNumber) {
-    for(let j = 0; j < (5 - starNumber); j++) {
-        el.innerHTML += `<span class="fa fa-star unchecked"></span>`;
-    }
-    for(let i = 0; i < starNumber; i++) {
-        el.innerHTML += `<span class="fa fa-star checked"></span>`;
-    }
-};
-
-let elementsFive = document.querySelectorAll('.abilities__skills--five-stars');
-
-elementsFive.forEach(function(current) {
-    starGenerator(current, 5);
-});
-
-let elementsFour = document.querySelectorAll('.abilities__skills--four-stars');
-
-elementsFour.forEach(function(current) {
-    starGenerator(current, 4);
-});
-
-let elementsThree = document.querySelectorAll('.abilities__skills--three-stars');
-
-elementsThree.forEach(function(current) {
-    starGenerator(current, 3);
-});
-
-let elementsTwo = document.querySelectorAll('.abilities__skills--two-stars');
-
-elementsTwo.forEach(function(current) {
-    starGenerator(current, 2);
-});
